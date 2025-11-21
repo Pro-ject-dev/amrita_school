cfd932ab1430138e0115cfd90db916456a76a219
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-11-21 16:54:27 +0530

    screen util added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:amrita_vidhyalayam_teacher/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -13,13 +14,21 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context ,WidgetRef ref) {
     final router = ref.read(routerProvider);
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Amrita Vidhyalayam Teacher App',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      routerConfig: router,
+    return  ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_ , child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Amrita Vidhyalayam Teacher App',
+          theme: ThemeData(
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          routerConfig: router,
+        );
+      }
     );
   }
 }
