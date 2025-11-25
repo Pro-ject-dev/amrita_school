10e5882165435dce8e9cc5ad993f22d4f62854b6
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-11-25 09:52:53 +0530

    splash screen design done

--- lib/core/features/splash_screen/presentation/view/splash_screen_page.dart
+++ lib/core/features/splash_screen/presentation/view/splash_screen_page.dart
@@ -1,6 +1,8 @@
 import 'package:amrita_vidhyalayam_teacher/core/features/splash_screen/presentation/viewmodel/splash_screen_state.dart';
+import 'package:amrita_vidhyalayam_teacher/core/theme/images/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import '../viewmodel/splash_screen_viewmodel.dart';
 
@@ -15,9 +17,20 @@ class SplashScreenPage extends ConsumerWidget {
       }
     });
 
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(AppImages.app_logo_primary, width: 200.w),
+            SizedBox(height: 10.h),
+            Text(
+              'Powered by My Amrita',
+              style: TextStyle(fontSize: 12.sp, color: Colors.grey.shade500,fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/core/theme/images/app_images.dart
+++ lib/core/theme/images/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String app_logo = 'assets/images/app_logo.png';
+  static const String app_logo_primary = 'assets/images/app_logo_primary.png';
   static const String home = 'assets/images/home.png';
 }
