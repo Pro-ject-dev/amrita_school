a735d3c6d51543a9c79a68610a7561bacbd00255
Pro-ject-dev <138247328+Pro-ject-dev@users.noreply.github.com>
2025-11-28 18:15:34 +0530

    student card UI updated

--- lib/core/features/home/presentation/widgets/student_card.dart
+++ lib/core/features/home/presentation/widgets/student_card.dart
@@ -1,27 +1,17 @@
 import 'package:amrita_vidhyalayam_teacher/core/theme/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class StudentCard extends StatelessWidget {
   final String name;
   final String id;
-
-  /// This MUST be final status: (localStatus > apiStatus > default)
   final String attendanceStatus;
-
   final String imageUrl;
-
-  /// Derived states
   final bool isPresent;
   final bool isAbsent;
-
-  /// Checkbox
   final bool isChecked;
-
-  /// Mode
   final bool isIndividual;
-
-  /// Callbacks
   final VoidCallback? onPresent;
   final VoidCallback? onAbsent;
   final ValueChanged<bool?>? onCheckboxChanged;
@@ -51,132 +41,150 @@ class StudentCard extends StatelessWidget {
 
     return Container(
       margin: EdgeInsets.only(bottom: 14.h),
-      padding: EdgeInsets.all(12.w),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16.r),
         border: Border.all(color: Colors.grey.shade300),
         color: Colors.white,
+        
       ),
-      child: Row(
+      
+      child: Stack(
         children: [
-          CircleAvatar(
-            radius: 24.r,
-            backgroundColor: Colors.grey.shade200,
-            child: CircleAvatar(
-              radius: 22.r,
-              backgroundImage: AssetImage("assets/images/student.gif"),
-            ),
-          ),
-
-          SizedBox(width: 12.w),
-
-          /// Name + ID
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  id,
-                  style: TextStyle(
-                    color: Colors.grey.shade700,
-                    fontSize: 13.sp,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          /// ---------------------------------------------------------
-          /// Buttons for Present / Absent when attendance is new
-          /// ---------------------------------------------------------
-          Visibility(
-            visible: showButtons,
+          Padding(
+            padding: EdgeInsets.all(12.w),
             child: Row(
               children: [
-                GestureDetector(
-                  onTap: onPresent,
+                
+                CircleAvatar(
+                  radius: 24.r,
+                  backgroundColor:attendanceStatus=="Present"?Colors.green:attendanceStatus=="Absent"?Colors.red: Colors.grey.shade200,
                   child: CircleAvatar(
-                    radius: 20.r,
-                    backgroundColor:
-                        isPresent ? Colors.green.withOpacity(0.15) : Colors.grey.shade200,
-                    child: Icon(
-                      Icons.check_circle,
-                      color: isPresent ? Colors.green : Colors.grey,
-                    ),
+                    radius: 22.r,
+                    backgroundImage: AssetImage("assets/images/student.gif"),
                   ),
                 ),
-
-                SizedBox(width: 10.w),
-
-                GestureDetector(
-                  onTap: onAbsent,
-                  child: CircleAvatar(
-                    radius: 20.r,
-                    backgroundColor:
-                        isAbsent ? Colors.red.withOpacity(0.15) : Colors.grey.shade200,
-                    child: Icon(
-                      Icons.cancel,
-                      color: isAbsent ? Colors.red : Colors.grey,
-                    ),
+            
+                SizedBox(width: 12.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        id,
+                        style: TextStyle(
+                          color: Colors.grey.shade700,
+                          fontSize: 13.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: showButtons,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: onPresent,
+                        child: CircleAvatar(
+                          radius: 20.r,
+                          backgroundColor:
+                              isPresent ? Colors.green.withOpacity(0.15) : Colors.grey.shade200,
+                          child: Icon(
+                            Icons.check_circle,
+                            color: isPresent ? Colors.green : Colors.grey,
+                          ),
+                        ),
+                      ),
+            
+                      SizedBox(width: 10.w),
+            
+                      GestureDetector(
+                        onTap: onAbsent,
+                        child: CircleAvatar(
+                          radius: 20.r,
+                          backgroundColor:
+                              isAbsent ? Colors.red.withOpacity(0.15) : Colors.grey.shade200,
+                          child: Icon(
+                            Icons.cancel,
+                            color: isAbsent ? Colors.red : Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: showCheckbox,
+                  child: Checkbox(
+                    value: isChecked,
+                    onChanged:onCheckboxChanged,
+                    fillColor: WidgetStateProperty.resolveWith((states) {
+                      return Colors.white;
+                    }),
+                    side: WidgetStateBorderSide.resolveWith((states) {
+                      return BorderSide(color: theme.primaryColor, width: 1);
+                    }),
+                    checkColor: theme.primaryColor,
+                    visualDensity: VisualDensity.comfortable,
+                  
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
                   ),
                 ),
               ],
             ),
           ),
 
-          /// ---------------------------------------------------------
-          /// Checkbox for bulk selection when attendance is new
-          /// ---------------------------------------------------------
-          
-          Visibility(
-            visible: showCheckbox,
-            child: Checkbox(
-              value: isChecked,
-              onChanged:onCheckboxChanged,
-              fillColor: WidgetStateProperty.resolveWith((states) {
-                return Colors.white;
-              }),
-              side: WidgetStateBorderSide.resolveWith((states) {
-                return BorderSide(color: theme.primaryColor, width: 1);
-              }),
-              checkColor: theme.primaryColor,
-              visualDensity: VisualDensity.comfortable,
-            
-              materialTapTargetSize: MaterialTapTargetSize.padded,
-            ),
-          ),
-          
-          
-
-          /// ---------------------------------------------------------
-          /// Final status (P / A) when already marked or after local change
-          /// ---------------------------------------------------------
           Visibility(
             visible: showFinalStatus,
-            child: CircleAvatar(
-              radius: 18.r,
-              backgroundColor:
-                  isPresent ? AppColors.primary : Colors.red.withOpacity(0.15),
-              child: CircleAvatar(
-                radius: 16.r,
-                backgroundColor: isPresent ? AppColors.primary : Colors.red,
-                child: Text(
-                  isPresent ? "P" : "A",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            child: Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.w,
+                    vertical: 3.h,
                   ),
-                ),
-              ),
-            ),
+                  decoration: BoxDecoration(
+                    gradient:attendanceStatus=="Present" ? LinearGradient(
+                                  colors: [Color(0xff4CAF50), Color(0xff204921)],
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                ):LinearGradient(
+                                  colors: [Color(0xffE53935), Color(0xff7F201D)],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                    
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16.r),
+                      bottomLeft: Radius.circular(12.r),
+                    ),
+                  ),
+                  child: Text(
+                   attendanceStatus,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
           ),
+
+          Visibility(
+            visible: showFinalStatus,child:Positioned(
+              bottom: 10,
+              right: 10,
+              child: attendanceStatus=="Present" ?   Icon(LucideIcons.badgeCheck500,color: Colors.green,size: 20.sp,):Icon(LucideIcons.badgeX,color: Colors.red,size: 20.sp,))),
+               
+                 
         ],
       ),
     );
